898bc50001cbeb4ff4caa662217f78974877521b
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-04-19 14:38:41 -0300

    final

--- modules/data_memory.v
+++ modules/data_memory.v
@@ -5,10 +5,10 @@ module Data_Memory #(parameter WORDSIZE = 64, parameter SIZE = 32) (
     input [WORDSIZE-1:0] data_input, /* valor a ser escrito na memória */
     input write_enable,   // habilita escrita
     input read, /* habilita leitura */
-    output [WORDSIZE-1:0]   read_data /* valor a ser lido, em caso de leitura */
+    output [WORDSIZE-1:0]   data_output /* valor a ser lido, em caso de leitura */
 );
     /* registradore em que serão armazenados os valores */
-    reg [SIZE-1:0] memory [WORDSIZE:0];
+    reg [WORDSIZE:0] memory [SIZE-1:0];
     
     /* ativação em borda de subida */
     always @(posedge clk) begin
@@ -18,6 +18,6 @@ module Data_Memory #(parameter WORDSIZE = 64, parameter SIZE = 32) (
     end
 
     /* saída de leitura */
-    assign read_data = (read==1'b1) ? memory[addr]: 64'd0; 
+    assign data_output = (read==1'b1) ? memory[addr]: 64'd0; 
 
 endmodule
--- testbenchs/test_data_memory.v
+++ testbenchs/test_data_memory.v
@@ -1,20 +1,44 @@
 `timescale 1ns/1ps
 
 /* módulo de teste para a rom */
-module test_data_memory;
-	reg [7:0] data_input;
-	wire [7:0] data_output;
+module test_data_memory #(parameter WORDSIZE = 64, parameter SIZE = 32) ;
+    reg clk;
+    reg [4:0] addr;
+    reg [WORDSIZE-1:0] data_input;
+    reg write_enable;
+    reg read;
+    wire [WORDSIZE-1:0] data_output;
 	
 	// instância da Unit Under Test
 	data_memory uut(
-		.data_input(data_input),
-		.data_output(data_output)
+		.clk(clk),
+		.addr(addr),
+        .write_enable(write_enable),
+        .read(read),
+        .data_output(data_output)
 	);
 	
 	// 
 	initial begin
-        data_input = 2;
-        $monitor ("data_input = %B, data_output = %B", data_input, data_output);
+        clk = 0;
+        addr = 5'b00101;
+        write_enable = 1;
+        read = 0;
+        $monitor ("clock = %B; addr = %B, write = %B; input = %B, output = %B", clk, addr, write_enable, data_input, data_output);
+        #100;
+
+        clk = 1;
+        addr = 5'b00101;
+        write_enable = 1;
+        read = 0;
+        $monitor ("clock = %B; addr = %B, write = %B; input = %B, output = %B", clk, addr, write_enable, data_input, data_output);
+        #100;
+
+        clk = 0;
+        addr = 5'b00101;
+        write_enable = 1;
+        read = 0;
+        $monitor ("clock = %B; addr = %B, write = %B; input = %B, output = %B", clk, addr, write_enable, data_input, data_output);
         #100;
 	end
 endmodule
