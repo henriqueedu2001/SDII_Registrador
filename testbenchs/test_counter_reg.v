c1052f497e485b4be3e9f997f2cccd2712da3675
julianamitie <juliana.mitiehn@gmail.com>
2023-05-03 10:58:25 -0300

    counter_reg e testbench funcionais

--- testbenchs/test_counter_reg.v
+++ testbenchs/test_counter_reg.v
@@ -1,10 +1,10 @@
-module test_counter_reg
+module test_counter_reg;
     reg clk; 
     reg reset; 
     wire [3:0] cr_data_output;
 
     // instância da Unit Under Test
-	counter uut(
+	counter_reg uut(
 		.clk(clk),
 		.reset(reset),
         .cr_data_output(cr_data_output));
@@ -13,52 +13,72 @@ module test_counter_reg
 
         clk = 0;
         reset = 1;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 1;
         reset = 1;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 0;
         reset = 1;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 1;
         reset = 0;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 0;
         reset = 0;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 1;
         reset = 0;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 0;
         reset = 0;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 1;
         reset = 0;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
+		#100;
+
+        clk = 0;
+        reset = 0;
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
+		#100;
+
+        clk = 1;
+        reset = 0;
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
+		#100;
+
+        clk = 0;
+        reset = 0;
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
+		#100;
+
+        clk = 1;
+        reset = 0;
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 0;
         reset = 1;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
         clk = 1;
         reset = 1;
-		$monitor ("clk = %B; reset = %B; out: %B", clk, reser, cr_data_output);
+		$monitor ("clk = %B; reset = %B; out: %B", clk, reset, cr_data_output);
 		#100;
 
     end
